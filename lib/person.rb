68a52cf9baf0d81ef8a178089774dce6ae3c5628
Nurina Junus <nurina.junus@gmail.com>
2022-02-16 12:21:04 -0700

    completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Person
 
+    attr_accessor :name
+
+    # setter method has an "=" appended to the name of the method
+    # the character identifies it as a setter method
+    def name=(name)
+        @name = name
+    end
+
+    # getter method
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
 end
+
